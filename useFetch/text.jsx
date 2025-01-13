1cb0c628a3340d0bf2fa4b2841b9f972801d9593
Cho Kar Hin <158465774+JokerHin@users.noreply.github.com>
2025-01-13 22:37:39 +0800

    Add files via upload

---
+++ useFetch/text.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import useFetch from ".";
+
+export default function UseFetchHookTest() {
+  const { data, error, pending } = useFetch(
+    "https://dummyjson.com/products",
+    {},
+  );
+
+  return (
+    <div>
+      <h1 className="mb-5 text-2xl font-bold">Use Fetch Hook</h1>
+      {pending ? <h3>Pending ! Please wait</h3> : null}
+      {error ? <h3>{error}</h3> : null}
+      {data && data.products && data.products.length
+        ? data.products.map((productItem) => (
+            <p key={productItem.id}>{productItem.title}</p>
+          ))
+        : null}
+    </div>
+  );
+}
